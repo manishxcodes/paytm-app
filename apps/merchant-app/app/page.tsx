d7c5ac4de64516aa344f632efcd7ec87c4cca47a
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-22 00:55:25 +0530

    initialise project: setup tuborepo with tailwind

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+        hello there, this is merchant app
+      </div>
+    </main>
+  );
+}
