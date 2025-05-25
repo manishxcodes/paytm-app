36814192f008457c8a958181f80342af899bd5c5
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-26 03:37:09 +0530

    move singleton prisma logic to client file

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -6,3 +6,5 @@ export const prisma =
   globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export * from "../generated/prisma/client";
