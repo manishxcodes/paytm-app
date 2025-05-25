c73c49d5259087c400166b355739e0409dc5cfbc
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-26 03:38:26 +0530

    feat: add login form

---
+++ apps/user-app/components/login-form.tsx
@@ -0,0 +1,103 @@
+"use client"
+
+import { cn } from "@repo/ui/lib/utils"
+import { Button } from "@repo/ui/components/shadcn/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/shadcn/card"
+import { Input } from "@repo/ui/components/shadcn/input"
+import { Label } from "@repo/ui/components/shadcn/label"
+import { useState } from "react"
+import { signIn } from "next-auth/react"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(email, password);
+    const response = await signIn("credentials", {
+      email, 
+      password,
+      redirect: true,
+      callbackUrl: "/"
+    });
+
+    if(response?.error) {
+      alert("user not found")
+      setError("invalid credentials");
+      console.log("error: user not found" )
+    }
+
+    if(response?.ok) {
+      console.log("ok")
+    }
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  onChange={(e) => {setEmail(e.target.value)}}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input 
+                  id="password" 
+                  type="password" 
+                  required 
+                  placeholder="Enter your password"
+                  onChange={(e) => {setPassword(e.target.value)}}
+                />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" onClick={handleSubmit}>
+                  Login
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
