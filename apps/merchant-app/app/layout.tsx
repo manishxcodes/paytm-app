41b736a692088a1e3ace3e2b054b6b491809ea52
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-26 01:30:02 +0530

    chore: wrap app with session provider

--- apps/merchant-app/app/layout.tsx
+++ apps/merchant-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { Provider } from "provider";
 
 const geist = Geist({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={geist.className}>{children}</body>
+      <Provider>
+        <body className={geist.className}>{children}</body>
+      </Provider>
     </html>
   );
 }
