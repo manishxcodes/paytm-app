072dbc099b2881d6f3d0b4e5e86ae753ceb7d2b3
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-27 02:16:54 +0530

    add user data types

--- apps/user-app/app/types/auth.types.ts
+++ apps/user-app/app/types/auth.types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type CredentialsTypes = {
     email: string;
     password: string;
@@ -10,3 +12,26 @@ export interface AuthUser {
     number: string
 }
 
+export const userDataSchema = z.object({
+  firstName: z
+    .string({ required_error: "First name is required" })
+    .min(1, { message: "First name cannot be empty" }),
+
+  lastName: z
+    .string({ required_error: "Last name is required" })
+    .min(1, { message: "Last name cannot be empty" }),
+
+  phoneNumber: z
+    .string({ required_error: "Phone number is required" })
+    .regex(/^\d{10}$/, { message: "Phone number must be 10 digits" }),
+
+  email: z
+    .string({ required_error: "Email is required" })
+    .email({ message: "Enter a valid email address" }),
+
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(8, { message: "Password must be at least 8 characters long" }),
+});
+
+export type UserData = z.infer<typeof userDataSchema>
