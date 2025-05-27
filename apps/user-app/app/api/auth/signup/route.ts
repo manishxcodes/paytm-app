5e328857c85fd18fc569a9eb80d0925492ce8445
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-05-28 02:49:08 +0530

    add signup route

--- apps/user-app/app/api/auth/signup/route.ts
+++ apps/user-app/app/api/auth/signup/route.ts
@@ -1,11 +1,57 @@
-import { NextResponse } from "next/server";
+import { prisma } from "@repo/db";
+import { userDataSchema } from "../../../types/auth.types";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt"
 
-export function GET() {
+export async function POST(req: NextRequest) {
     // get all the inputs 
+    const body = await req.json();
+    
+    // zod validation
+    const response = userDataSchema.safeParse(body);
+    if(!response) {
+        return NextResponse.json({message: "Invalid credentials"});
+    }
+
     // check if user exists 
-    // if exists then tell them to signin
-    // if not 
-    //      - verify otp
-    //      -create new user
+    const existingUser = await prisma.user.findFirst({
+        where: {
+            email: body.email
+        }
+    });
+        // if exists then tell them to signin
+    if(existingUser) {
+        return NextResponse.json({message: "This email has already been registered by an user. Please signin"});
+    }
+
+    // hashpassword
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(body.password, salt);
+
+    // if new user create account
+    try {
+        console.log({body: body});
+        const res = await prisma.user.create({
+            data: {
+                firstName: body.firstName,
+                lastName: body.lastName,
+                email: body.email,
+                number: body.number,
+                password: hashedPassword,
+            }
+        });
+        if(res) {
+            return NextResponse.json({message: "User Created Successfully"});
+        }
+    } catch(err) {
+        console.log("Failed to signup", {details: err});
+        return NextResponse.json({message: "Failed to Signup"}, {status: 500});
+    }
+
+    return NextResponse.json({message: "Something went wrong"}, {status: 500});
+}
+
+export async function GET(req: NextRequest) {
     return NextResponse.json({hello: "there"})
 }
