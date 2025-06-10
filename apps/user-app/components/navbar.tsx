e5135e3e1df2d191b3a20981b32223bd5658d02c
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-06-10 23:38:19 +0530

    chore: add conditional signout button

--- apps/user-app/components/navbar.tsx
+++ apps/user-app/components/navbar.tsx
@@ -6,23 +6,34 @@ import { signIn, signOut, useSession } from "next-auth/react"
 import Link from "next/link"
 
 export function Navbar() {
-  return (
-    <div className="bg-background shadow-accent-foreground">
-        <Container>
-            <div className="flex items-center justify-between py-4">
-            <div className="font-semibold">PayTm </div>
-                <div className="flex gap-4">
-                    <Button size={"sm"} variant={"outline"} 
-                        onClick={() => {signIn()}}>Sign In</Button>
-                    <Link href={"/auth/signup"}>
-                        <Button className="bg-accent hover:bg-accent/70 hover:scale-95 text-accent-foreground transition-all duration-150" size={"sm"}
-                    >Register</Button>
-                    </Link>
-                </div>
+    const {data: session, status} = useSession();
 
-                {/* make simple navbar using login only and for signup we can do in cta */}
-            </div>
-        </Container>
-    </div>
-  )
+    return (
+        <div className="bg-background shadow-accent-foreground">
+            <Container>
+                <div className="flex items-center justify-between py-4">
+                <div className="font-semibold">PayTm </div>
+                {status === "loading" ? null
+                    :session 
+                    ? <div className="flex gap-4">
+                        <Button size={"sm"} variant={"outline"} 
+                            onClick={() => {signOut({callbackUrl: "/"})}}>Sign Out
+                        </Button>
+                    </div>
+                    : <div className="flex gap-4">
+                        <Button size={"sm"} variant={"outline"} 
+                            onClick={() => {signIn()}}>Sign In</Button>
+                        <Link href={"/auth/signup"}>
+                            <Button className="bg-accent hover:bg-accent/70 hover:scale-95 text-accent-foreground transition-all duration-150" size={"sm"}
+                        >Register</Button>
+                        </Link>
+                    </div>
+                }
+                    
+
+                    {/* make simple navbar using login only and for signup we can do in cta */}
+                </div>
+            </Container>
+        </div>
+    )
 }
