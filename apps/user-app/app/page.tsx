d48e3dc19a4221f4db849e69b5c6af25cc17570d
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-23 23:56:33 +0530

    chore: add home page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,17 +1,11 @@
-import { Card } from "@repo/ui/components/card";
-
-import { prisma } from "@repo/db";
-import { InputOTP } from "@repo/ui/components/ui/input-otp";
+import { Navbar } from "@components/navbar";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        hello there, this is user app
+    <main className="flex flex-col w-full">
+      <div>
+        <Navbar  />
       </div>
-      <Card title={"Yo"}
-        children={<button>Gurt</button>}
-        href={"click here"} />
     </main>
   );
 }
