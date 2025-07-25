d23b87ca368e05227df00dc07ca756a55dd48c83
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-25 21:57:00 +0530

    chore: redirect to signin page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,8 +6,10 @@ import { redirect } from "next/navigation";
 export default async function Page() {
   const session = await getServerSession(authOptions);
   
-  if(session) {
+  if(session?.user) {
     redirect('/dashboard');
+  } else {
+    redirect('/api/auth/signin');
   }
 
   return (
