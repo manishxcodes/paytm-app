6a59df109d37301c976ad238ea020e99e36b9bcd
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-25 21:54:58 +0530

    chore: redirect to dashboard

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,15 @@
 import { Navbar } from "@components/navbar";
+import { authOptions } from "@lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  
+  if(session) {
+    redirect('/dashboard');
+  }
+
   return (
     <main className="flex flex-col w-full">
       <div>
