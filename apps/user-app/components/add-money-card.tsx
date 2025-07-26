c70b38767f2338317b9d5bceb2cc49cc5f276b65
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-07-26 20:55:44 +0530

    chore: integrate onramptransaction

--- apps/user-app/components/add-money-card.tsx
+++ apps/user-app/components/add-money-card.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { createOnRampTransaction } from "@lib/actions/create-on-ramp-transaction";
 import { Select } from "@repo/ui/components/select";
 import { Button } from "@repo/ui/components/shadcn/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@repo/ui/components/shadcn/card";
@@ -17,6 +18,7 @@ const SUPPORTED_BANKS = [{
 export function AddMoney() {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
     const [amount, setAmount] = useState<number>();
+    const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name);
 
     return (
         <Card className="w-full min-w-lg min-h-[200]">
@@ -28,13 +30,23 @@ export function AddMoney() {
                 <Input className="mb-2"
                 value={amount} type='number' placeholder="Enter amount" onChange={(e) => setAmount(Number(e.target.value))} />
                 <Select onSelect={(value) => {
-                    setRedirectUrl(value);
+                    const selectedBank = SUPPORTED_BANKS.find(bank => bank.redirectUrl == value);
+                    setRedirectUrl(selectedBank?.redirectUrl),
+                    setProvider(selectedBank?.name);
                 }} options={SUPPORTED_BANKS.map(x => 
                     ({
                         key: x.name, value: x.redirectUrl
                     }))} />
                 <div className="flex justify-center pt-4">
-                    <Button onClick={() => {window.location.href = redirectUrl || ""}}>
+                    <Button onClick={async () => {
+                        if (!provider || !amount || amount <= 0) {
+                            alert("Please select a bank and enter a valid amount.");
+                            return;
+                        }
+
+                        await createOnRampTransaction(provider, amount)
+                        window.location.href = redirectUrl || "";
+                    }}>
                         Add Money
                     </Button>
                 </div>
